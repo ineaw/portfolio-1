dbc3f920edcd5ce2b6f434fa9244db4ba55c9524
ineaw <ineaw@users.noreply.github.com>
2021-06-05 19:47:15 +0200

    testing for mobile, just a draft

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ const btn = document.querySelector(".btn-toggle");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
 const currentTheme = localStorage.getItem("theme");
-if (currentTheme == "dark") {
+if (currentTheme === "dark") {
   document.body.classList.toggle("dark-theme");
-} else if (currentTheme == "light") {
+} else if (currentTheme === "light") {
   document.body.classList.toggle("light-theme");
 }
 
